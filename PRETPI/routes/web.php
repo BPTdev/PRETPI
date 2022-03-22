2eb149e42df8735cb640dcfd5b45fe87e2c53f42
Benoit <47849605+cpnvbenoit@users.noreply.github.com>
2022-03-22 09:30:08 +0100

    update preop

--- PRETPI/routes/web.php
+++ PRETPI/routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
